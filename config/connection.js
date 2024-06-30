fa53c2c67b0c12bcdf89bbd07bcc019b44ef50e8
David Rauf <davidrauf@gmail.com>
2024-06-30 12:22:05 -0400

    server running

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,16 @@
-require('dotenv').config();
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.POSTGRESURI);
+const sequelize = process.env.POSTGRESURI
+  ? new Sequelize(process.env.POSTGRESURI)
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    }
+  );
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/routes'));
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+});
